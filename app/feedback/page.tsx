07d33927c109b3c9463d47a5260de5879ded740d
Danielle Castaneda <danielleclaudettecastaneda@gmail.com>
2024-10-20 03:09:55 +0800

    build fix

--- app/feedback/page.tsx
+++ app/feedback/page.tsx
@@ -3,14 +3,9 @@ import { sql } from '@vercel/postgres';
 export const fetchCache = 'force-no-store';
 
 export default async function Adminlogin() {
-  const res = await sql`
-      SELECT * FROM feedbacks LIMIT 1000000
-  `;
-  const feedbacks = res.rows;
-
   return (
     <>
-      <AdminFeedbackPage feedbacks={feedbacks} />
+      <AdminFeedbackPage />
     </>
   );
 }
--- components/AdminFeedbackPage.tsx
+++ components/AdminFeedbackPage.tsx
@@ -3,17 +3,11 @@ import React from 'react';
 import FeedbackList from './feedbackList';
 import ScrollToTopButton from './ScrollToTopButton';
 
-export default function AdminFeedbackPage({
-  feedbacks,
-}: {
-  feedbacks: QueryResultRow[];
-}) {
+export default function AdminFeedbackPage() {
   return (
     <section
       id='feedbackPage'
-      className={`relative bg-darkTeal-bgColor flex px-20 lg:py-20 pt-28 justify-start gap-14 items-center flex-col ${
-        feedbacks.length > 2 ? 'h-fit' : 'h-lvh'
-      }`}
+      className={`relative bg-darkTeal-bgColor flex px-20 lg:py-20 pt-28 justify-start gap-14 items-center flex-col`}
     >
       <div className='relative flex font-bold text-white-headline_titles_description text-[30px] md:text-[40px] lg:text-[60px] justify-center items-center mt-14 xl:mt-10 drop-shadow-md'>
         Feedbacks and Suggestions
